212ef63fda25599377830204fd7a7fdae7eb455c
kiyomikanaike <kiyomi.kanaike@gmail.com>
2023-05-05 05:12:50 +0000

    Save

---
+++ app/controllers/public/items_controller.rb
@@ -0,0 +1,16 @@
+class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.page(params[:page])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:image,:price)
+  end
+end
