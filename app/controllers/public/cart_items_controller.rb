84c560604113bbcbfa667e3fadaf51702078f54d
kiyomikanaike <kiyomi.kanaike@gmail.com>
2023-05-10 12:01:29 +0000

    Save

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,34 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    
+  end
+  
+  
+  def update
+    
+  end
+  
+  
+  def destroy
+    
+  end
+  
+  
+  def destroy_all
+    
+  end
+  
+  
+  def create
+    @cart_item = Cart_item.new(cart_item_params)
+    @item.save
+    redirect_to #ショッピングカートページ(@cart_item.id)
+  end
+  
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
+    
 end
